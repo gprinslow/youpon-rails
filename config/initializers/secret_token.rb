10984d7d631e90d2a0748701eb0ab978104a5d0d
Garrison Prinslow <gprinslow@gmail.com>
2011-11-18 13:44:14 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youpon::Application.config.secret_token = '5392d3653178076d5b0a24b7eb7366c47d43b2a689d2207cc05b944ef4bb33c9d190f939749f10635323d334a5e2f6a7ca6c599c9ba9fde8163ee16be99b4d09'
