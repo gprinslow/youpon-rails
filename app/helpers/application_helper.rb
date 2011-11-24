437a2eed5a1a3535e9f44adc9594cf653edab3c7
Garrison Prinslow <gprinslow@gmail.com>
2011-11-23 22:46:33 -0600

    Title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  #Return a title
+  def title
+    base_title = "Youpon"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
