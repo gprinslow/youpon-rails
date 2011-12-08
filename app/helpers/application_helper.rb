b8e093585dcf6213bae3c3cf71b0e9223deb1724
Garrison Prinslow <gprinslow@gmail.com>
2011-12-08 17:04:22 -0600

    Add routes and logo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,16 @@
 module ApplicationHelper
   
-  #Return a title
+	#Helper to return the logo
+	def logo
+		logo = image_tag("logo.png", :alt => "Youpon", :class =>"round")
+		if @logo.nil?
+			logo
+		else
+			"Youpon"
+		end
+	end
+
+  #Return a title for each page
   def title
     base_title = "Youpon"
     if @title.nil?
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Youpon::Application.routes.draw do
 	
-	root :to => 'pages#home'
+	root 							:to => 'pages#home'
 	
 	match '/contact',	:to => 'pages#contact'
 	match '/about',		:to => 'pages#about'
