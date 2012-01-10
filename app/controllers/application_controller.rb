7ca3946eacc4e5b110b12f592354553b739de13b
Garrison Prinslow <gprinslow@gmail.com>
2012-01-10 11:16:16 -0600

    New mac mini repo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
 	#This forces https in test and production
 	force_ssl
 	
+	#This is a fugly way to keep the app somewhat private
+	http_basic_authenticate_with :name => "gprinslow", :password => "asdfFoobar1234"
 	
-	
+	include SessionsHelper
 	private
 	
 	def current_user
