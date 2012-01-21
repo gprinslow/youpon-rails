e527f490863e7458e8f6d3fa20c736f23f53c916
Garrison Prinslow <gprinslow@gmail.com>
2012-01-21 17:15:52 -0600

    Add message responses to User for JSON service

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,87 @@
+require "rexml/document"
+
 class UsersController < ApplicationController
+
+  # GET /users/1
+  # GET /users/1.xml
+  def show
+    @user = User.find(params[:id])
+    @title = @user.email
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+      format.json { render :json => {:items => @user }}
+    end
+	end
+
+  # GET /users/new
+  # GET /users/new.xml
   def new
-		@title = "Sign up"
-		@user = User.new
-		
-		respond_to do |format|
-			format.html	# new.html.erb
-			format.xml 	{ render :xml => @user }
-		end
+    @title = "Sign up"
+    @user = User.new
+    
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
+  
+  # GET /users/1/edit
+	def edit
+	  @user = User.find(params[:id])
+	end
+
+  # POST /users
+  # POST /users.xml
+  def create
+    @user = User.new(params[:user])
+    
+    respond_to do |format|
+      if @user.save
+        format.html { sign_in @user; flash[:success] = "Welcome to Youpon!"; redirect_to(@user) }
+        format.xml  { render :xml => @user, :status => :created, :location => @user }
+        format.json { sign_in @user; render :json => {:items => @user}, :status => :created, :location => @user }
+      else
+        @title = "Sign up"
+        @user.password = ""
+        @user.password_confirmation = ""
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @user.errors,  :status => :unprocessable_entity }
+				format.json { render :json => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  # PUT /users/1
+  # PUT /users/1.xml
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
+        format.xml  { head :ok }
+				format.json { render :json => @user, :status => :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+				format.json { render :json => @user.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
-	def create
-		@user = User.new(params[:user])
-		if @user.save
-			sign_in @user
-			flash[:success] = "Welcome to Youpon!"
-			redirect_to @user
-		else
-			@title = "Sign up"
-			render 'new'
-		end
-	end
-	
-	def show
-		@user = User.find(params[:id])
-		@title = @user.email
-	end
+  # DELETE /users/1
+  # DELETE /users/1.xml
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(users_url) }
+      format.xml  { head :ok }
+			format.json { render :json => @user, :status => :ok }
+    end
+  end
 	
 end
