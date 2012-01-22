b8e544b948dc1f83d201624e3e4ccf8b3bba07df
Garrison Prinslow <gprinslow@gmail.com>
2012-01-21 18:15:16 -0600

    Added layout integration tests signin/out

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
+  describe "at any point the app" do
 	  it "should have a Home page at '/'" do
 	    get '/'
 	    response.should have_selector('title', :content => "Home")
@@ -40,4 +41,30 @@ describe "LayoutLinks" do
 	    click_link "Sign up now!"
 	    response.should have_selector('title', :content => "Sign up")
 	  end
+	end  
+	
+  describe "when not signed in" do
+    it "should have a signin link" do
+      visit root_path
+      response.should have_selector("a", :href => signin_path, :content => "Sign in")
+    end
+  end
+
+  describe "when signed in" do	
+		before(:each) do
+			@user = Factory(:user)
+			integration_sign_in(@user)
+		end
+		
+		it "should have a signout link" do
+			visit root_path
+			response.should have_selector("a", :href => signout_path, :content => "Sign out")
+		end
+		
+		it "should have a profile link" do
+			visit root_path
+			response.should have_selector("a", :href => user_path(@user), :content => "Profile")
+		end
+  end
+  
 end
