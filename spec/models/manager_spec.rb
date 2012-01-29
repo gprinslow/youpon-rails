a56eccb6f3e03176c02ac038c2e1f34b3264bd4f
Garrison Prinslow <gprinslow@gmail.com>
2012-01-29 17:40:02 -0600

    Manager working & spec'd

--- spec/models/manager_spec.rb
+++ spec/models/manager_spec.rb
@@ -1,5 +1,61 @@
 require 'spec_helper'
 
 describe Manager do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @user_attr = {  :name => "autotest01", 
+                    :email => "autotest01@example.com", 
+                    :password => "foobar", 
+                    :password_confirmation => "foobar" }
+	end
+	
+	it "should create a new instance given valid attributes" do
+		@manager = Manager.create!
+		@user = @manager.create_role.create_user(@user_attr)
+	end
+	
+	describe "role associations" do
+	  
+	  before(:each) do
+	    @manager = Manager.create!
+      @user = @manager.create_role.create_user(@user_attr)
+    end
+    
+    it "should have a role attribute" do
+      @manager.should respond_to(:role)
+    end
+    
+    it "should have the right associated role" do
+      @manager.role_id == @manager.role.id
+    end
+    
+    it "should have a user attribute" do
+      @manager.role.should respond_to(:user)
+    end
+    
+    it "should have the right associated user through role" do
+      @manager.role.user_id.should == @user.id
+      @manager.role.user.should == @user
+    end
+  end
+  
+  describe "distinguish from an employee" do
+    before(:each) do
+      @manager = Manager.create!
+      @employee = Employee.create!
+      @user = @manager.create_role.create_user(@user_attr)
+    end
+     
+    it "should identify its subclass" do
+      @manager.class.should == Manager
+    end
+     
+    it "should identify its parent class" do
+      @manager.is_a?(Employee).should be_true
+    end
+     
+    it "should distinguish between managers and non-managers" do
+      @employee.is_a?(Manager).should_not be_true
+    end
+  end
 end
