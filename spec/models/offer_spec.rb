684af2fae3fb3f7a4f9d33bee03b9cedd4903ad2
Garrison Prinslow <gprinslow@gmail.com>
2012-01-29 23:28:53 -0600

    Offer model spec'd OK

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Offer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @attr = {
+      :title => "title",
+      :byline => "byline",
+      :category => "test coupons",
+      :discount => "$1",
+      :description => "ineffable",
+      :terms => "no terms!",
+      :start => Time.now,
+      :end => Time.now,
+      :number_offered => 10,
+      :validation_required => true 
+      }
+  end
+  
+  it "should create a new instance given valid attributes" do
+    @offer = Offer.create!(@attr)
+  end
+  
+  describe "merchant association" do
+    
+    before(:each) do
+      @offer = Offer.create!(@attr)
+    end
+    
+    it "should have a merchant attribute" do
+      @offer.should respond_to(:merchant)
+    end
+  end
 end
