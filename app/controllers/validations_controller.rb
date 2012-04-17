c390b3845774da33c9071aff11b94f080bd496b4
Garrison Prinslow <gprinslow@gmail.com>
2012-04-17 15:22:46 -0500

    Redeem-validate works properly with iPhone

--- app/controllers/validations_controller.rb
+++ app/controllers/validations_controller.rb
@@ -15,6 +15,12 @@ class ValidationsController < ApplicationController
           if @offer.validation_required?
             @key_match = Key.find_by_code(params[:validation_request][:key_entry])
             if @key_match
+              @validation_attr = { :criteria => "entered key == offer.merchant.keys?",
+                                   :match => "entered key == offer.merchant.keys" }
+              
+              @validation = Validation.new(@validation_attr)
+              @validation.request = @request
+              
               #find the employee who validated
               @employee = Employee.find_by_id(@key_match.employee_id)
               @validation.employee = @employee
@@ -22,43 +28,53 @@ class ValidationsController < ApplicationController
               @new_r_status = RequestStatus.find_by_id(2)
               @request.status = @new_r_status
               @validation.status = @new_v_status
-              #@validation.critera = "entered key == offer.merchant.keys?"
-              #@validation.match = "entered key == offer.merchant.keys"
               @request.save!
               @validation.save!
               render :json => { :validation => @validation }
             else #key did not match - invalid
+              @validation_attr = { :criteria => "key == entered_key?",
+                                   :match => "key x= entered_key" }
+              
+              @validation = Validation.new(@validation_attr)
+              @validation.request = @request
+              
               @new_v_status = ValidationStatus.find_by_id(6)
               @new_r_status = RequestStatus.find_by_id(3)
               @request.status = @new_r_status
               @validation.status = @new_v_status
-              @validation.critera = "key == entered_key?"
-              @validation.match = "key x= entered_key"
               @request.save!
               @validation.save!
-              render :json => { :errors => @new_v_status } 
+              render :json => { :errors => { :error => @new_v_status } } 
             end
           else  #validation is not required - automatically valid
+            @validation_attr = { :criteria => "no validation required",
+                                 :match => "no validation required" }
+            
+            @validation = Validation.new(@validation_attr)
+            @validation.request = @request
+            
             @new_v_status = ValidationStatus.find_by_id(5)
             @new_r_status = RequestStatus.find_by_id(2)
             @request.status = @new_r_status
             @validation.status = @new_v_status
-            @validation.critera = "no validation required"
-            @validation.match = "no validation required"
             @request.save!
             @validation.save!
             render :json => { :validation => @validation }
           end
         else #invalid request - not new
+          @validation_attr = { :criteria => "request.status == new",
+                               :match => "request.status x= new" }
+          
+          @validation = Validation.new(@validation_attr)
+           @validation.request = @request
+           
            @new_v_status = ValidationStatus.find_by_id(6)
            @new_r_status = RequestStatus.find_by_id(3)
            @request.status = @new_r_status
            @validation.status = @new_v_status
-           @validation.critera = "request.status == new"
-           @validation.match = "request.status x= new"
            @request.save!
            @validation.save!
-           render :json => { :errors => @new_v_status }
+           render :json => { :errors => { :error => @new_v_status } }
         end                 
 		  }
 		end
