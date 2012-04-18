f86c7328b608ed766ee10b9f30ea6bda5c589fcf
Garrison Prinslow <gprinslow@gmail.com>
2012-04-18 15:21:05 -0500

    first attempt at combined merchant controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,14 @@
 class MerchantsController < ApplicationController
   def new
+    @title = "Merchant Sign up"
+    @merchant = Merchant.new
+    @location = MerchantLocation.new
+    @merchant.location = @location
+    
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @merchant }
+    end
   end
 
 end
